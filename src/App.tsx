be13144a523039e4ab67ae3c560c895c685444c9
Eze Muñoz <eze.munoz@mi.unc.edu.ar>
2023-03-29 19:17:50 -0300

    testing how to use env variables with netlify

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,6 @@ import { useGetIpData } from './hooks/useGetIpData';
 function App(): JSX.Element {
   const [ip, setIp] = useState<string>('');
   const { ipData, loading, getIpData } = useGetIpData();
-  console.log(import.meta.env.VITE_RAPID_API_KEY);
-
-  
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => { 
     e.preventDefault();
